ba036647f9d5ac05abbe0b4bf3d13ecc6766464b
Enzo Piriou <enzopiriou@gmail.com>
2022-05-24 04:55:46 +0200

    flex guide

--- src/App.svelte
+++ src/App.svelte
@@ -17,49 +17,63 @@
 {#if page === "#tech_editor"}
   <TechEditor />
 {:else if page === "#guide"}
-  <h2>Starting Units</h2>
-  <ul>
-    <li>1 Dreadnought</li>
-    <li>1 Carrier</li>
-    <li>3 Fighter</li>
-    <li>5 Infantry</li>
-    <li>1 Space Dock</li>
-    <li>1 PDS</li>
-  </ul>
+  <div class="guide">
+    <div class="container">
+      <h2>Starting Units</h2>
+      <ul>
+        <li>1 Dreadnought</li>
+        <li>1 Carrier</li>
+        <li>3 Fighter</li>
+        <li>5 Infantry</li>
+        <li>1 Space Dock</li>
+        <li>1 PDS</li>
+      </ul>
+    </div>
 
-  <h2>Starting Techs</h2>
-  <ul>
-    <li>Neural Motivator</li>
-    <li>Plasma Scoring</li>
-  </ul>
+    <div class="container">
+      <h2>Starting Techs</h2>
+      <ul>
+        <li>Neural Motivator</li>
+        <li>Plasma Scoring</li>
+      </ul>
+    </div>
 
-  <h2>Faction Abilities</h2>
-  <ul>
-    <li>
-      <strong>ASSIMILATE</strong>: When you gain control of a planet, replace
-      each PDS and space dock that is on that planet with a matching unit from
-      your reinforcements.
-    </li>
-    <li>
-      <strong>HARROW</strong>: At the end of each round of ground combat, your
-      ships in the active system may use their Bombardment abilities against
-      your opponent's ground forces on the planet.
-    </li>
-  </ul>
+    <div class="container">
+      <h2>Faction Abilities</h2>
+      <ul>
+        <li>
+          <strong>ASSIMILATE</strong>: When you gain control of a planet,
+          replace each PDS and space dock that is on that planet with a matching
+          unit from your reinforcements.
+        </li>
+        <li>
+          <strong>HARROW</strong>: At the end of each round of ground combat,
+          your ships in the active system may use their Bombardment abilities
+          against your opponent's ground forces on the planet.
+        </li>
+      </ul>
+    </div>
 
-  <h2>Factions Techs</h2>
-  <ul>
-    <li>Inhericance Systems</li>
-  </ul>
+    <div class="container">
+      <h2>Factions Techs</h2>
+      <ul>
+        <li>Inhericance Systems</li>
+      </ul>
+    </div>
 
-  <h2>Faction Specific Units</h2>
-  <ul>
-    <li>Super-DreadNought I</li>
-    <li>Super-DreadNought II</li>
-  </ul>
+    <div class="container">
+      <h2>Faction Specific Units</h2>
+      <ul>
+        <li>Super-DreadNought I</li>
+        <li>Super-DreadNought II</li>
+      </ul>
+    </div>
 
-  <h2>Faction Specific Units</h2>
-  <p>L1Z1X Flagship</p>
+    <div class="container">
+      <h2>Faction Specific Units</h2>
+      <p>L1Z1X Flagship</p>
+    </div>
+  </div>
 
   <textarea>{JSON.stringify($techsStore)}</textarea>
 {:else}
@@ -70,6 +84,18 @@
   :root {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  .guide {
+    display: flex;
+    flex-wrap: wrap;
+    & > .container {
+      max-width: 30vw;
+      border: 1px solid black;
+      /* border-radius: 10px; */
+      padding: 1rem;
+      margin: 0.2rem;
+    }
   }
 
   textarea {
