4351564d2e79c3801d91a11bb37b1a8664cdac5e
Enzo Piriou <enzopiriou@gmail.com>
2022-05-24 18:04:46 +0200

    now get Strategy cards from backend

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,10 @@
 import type { Faction, TechType } from "./types/tech.types";
 
 export const techTypes: TechType[] = [
-	"Biotic",
-	"Cybernetic",
-	"Propulsion",
-	"Warfare",
+  "Biotic",
+  "Cybernetic",
+  "Propulsion",
+  "Warfare",
 ];
 export const factions: Faction[] = ["The Arborec"];
 
--- src/pages/StrategyCards.svelte
+++ src/pages/StrategyCards.svelte
@@ -1,113 +1,123 @@
 <script lang="ts">
   import type { Strat } from "src/types/tech.types";
+  import { onMount } from "svelte";
+  import { getStrategies } from "../api";
 
-  const cards: Strat[] = [
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Leadership_(Strategy_card)",
-      name: "Leadeship",
-      color: "red",
-      initiative: 1,
-      primary_abilities: [
-        "Gain 3 command tokens.",
-        "Spend any amount of influence to gain 1 command token for every 3 influence spent",
-      ],
-      secondary_abilities: [
-        "Spend any amount of influence to gain 1 command token for every 3 influence spent",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Diplomacy_(Strategy_card)",
-      name: "Diplomacy",
-      color: "orange",
-      initiative: 2,
-      primary_abilities: [
-        "Choose 1 system other than the Mecatol Rex system that contains a planet you control; each other player places a command token from their reinforcements in the chosen system. Then, ready up to 2 exhausted planets you control.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool to ready up to 2 exhausted planets you control.",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Politics_(Strategy_card)",
-      name: "Politics",
-      color: "yellow",
-      initiative: 3,
-      primary_abilities: [
-        "Choose a player other than the speaker.  That player gains the speaker token.",
-        "Draw 2 action cards.",
-        "Look at the top 2 cards of the agenda deck.  Place each card on the top or bottom of the deck in any order.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool to draw 2 action cards.",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Construction_(Strategy_card)",
-      name: "Constuction",
-      color: "green",
-      initiative: 4,
-      primary_abilities: [
-        "Place 1 PDS or 1 Space Dock on a planet you control.",
-        "Place 1 PDS on a planet you control.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool and place it in any system; you may place either 1 space dock or 1 PDS on a planet you control in that system.",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Trade_(Strategy_card)",
-      name: "Trade",
-      color: "turquoise",
-      initiative: 5,
-      primary_abilities: [
-        "Gain 3 trade goods.",
-        "Replenish commodities.",
-        "Choose any number of other players. Those players use the secondary ability of this strategy card without spending a command token.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool to replenish your commodities.",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Warfare_(Strategy_card)",
-      name: "Warfare",
-      color: "dodgerblue",
-      initiative: 6,
-      primary_abilities: [
-        "Remove 1 of your command tokens from the game board; then, gain 1 command token.",
-        "Redistribute any number of the command tokens on your command sheet.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool to use the Production ability of 1 of your space docks in your home system (This token is not placed in your home system)",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Technology_(Strategy_card)",
-      name: "Technology",
-      color: "darkblue",
-      initiative: 7,
-      primary_abilities: [
-        "Research 1 technology.",
-        "Spend 6 resources to research 1 technology.",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool and 4 resources to research 1 technology.",
-      ],
-    },
-    {
-      link: "https://twilight-imperium.fandom.com/wiki/Imperial_(Strategy_card)",
-      name: "Imperial",
-      color: "purple",
-      initiative: 8,
-      primary_abilities: [
-        "Immediately score 1 public objective if you fulfill its requirements.",
-        "Gain 1 victory point if you control Mecatol Rex; otherwise, draw 1 secret objective",
-      ],
-      secondary_abilities: [
-        "Spend 1 token from your strategy pool to draw 1 secret objective.",
-      ],
-    },
-  ];
+  let cards: Strat[] = [];
+
+  onMount(async () => {
+    const res = await getStrategies();
+    console.log("res:", res);
+    cards = res;
+  });
+
+  // const cards: Strat[] = [
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Leadership_(Strategy_card)",
+  //     name: "Leadeship",
+  //     color: "red",
+  //     initiative: 1,
+  //     primary_abilities: [
+  //       "Gain 3 command tokens.",
+  //       "Spend any amount of influence to gain 1 command token for every 3 influence spent",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend any amount of influence to gain 1 command token for every 3 influence spent",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Diplomacy_(Strategy_card)",
+  //     name: "Diplomacy",
+  //     color: "orange",
+  //     initiative: 2,
+  //     primary_abilities: [
+  //       "Choose 1 system other than the Mecatol Rex system that contains a planet you control; each other player places a command token from their reinforcements in the chosen system. Then, ready up to 2 exhausted planets you control.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool to ready up to 2 exhausted planets you control.",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Politics_(Strategy_card)",
+  //     name: "Politics",
+  //     color: "yellow",
+  //     initiative: 3,
+  //     primary_abilities: [
+  //       "Choose a player other than the speaker.  That player gains the speaker token.",
+  //       "Draw 2 action cards.",
+  //       "Look at the top 2 cards of the agenda deck.  Place each card on the top or bottom of the deck in any order.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool to draw 2 action cards.",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Construction_(Strategy_card)",
+  //     name: "Constuction",
+  //     color: "green",
+  //     initiative: 4,
+  //     primary_abilities: [
+  //       "Place 1 PDS or 1 Space Dock on a planet you control.",
+  //       "Place 1 PDS on a planet you control.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool and place it in any system; you may place either 1 space dock or 1 PDS on a planet you control in that system.",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Trade_(Strategy_card)",
+  //     name: "Trade",
+  //     color: "turquoise",
+  //     initiative: 5,
+  //     primary_abilities: [
+  //       "Gain 3 trade goods.",
+  //       "Replenish commodities.",
+  //       "Choose any number of other players. Those players use the secondary ability of this strategy card without spending a command token.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool to replenish your commodities.",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Warfare_(Strategy_card)",
+  //     name: "Warfare",
+  //     color: "dodgerblue",
+  //     initiative: 6,
+  //     primary_abilities: [
+  //       "Remove 1 of your command tokens from the game board; then, gain 1 command token.",
+  //       "Redistribute any number of the command tokens on your command sheet.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool to use the Production ability of 1 of your space docks in your home system (This token is not placed in your home system)",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Technology_(Strategy_card)",
+  //     name: "Technology",
+  //     color: "darkblue",
+  //     initiative: 7,
+  //     primary_abilities: [
+  //       "Research 1 technology.",
+  //       "Spend 6 resources to research 1 technology.",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool and 4 resources to research 1 technology.",
+  //     ],
+  //   },
+  //   {
+  //     link: "https://twilight-imperium.fandom.com/wiki/Imperial_(Strategy_card)",
+  //     name: "Imperial",
+  //     color: "purple",
+  //     initiative: 8,
+  //     primary_abilities: [
+  //       "Immediately score 1 public objective if you fulfill its requirements.",
+  //       "Gain 1 victory point if you control Mecatol Rex; otherwise, draw 1 secret objective",
+  //     ],
+  //     secondary_abilities: [
+  //       "Spend 1 token from your strategy pool to draw 1 secret objective.",
+  //     ],
+  //   },
+  // ];
 </script>
 
 <div id="main">
